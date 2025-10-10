58ce79049ab096526feeaa5454c9138c52a41a1b
mazensomran <141928533+mazensomran@users.noreply.github.com>
2025-10-10 13:47:33 +0300

    Update activity_recognizer.py

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -1,4 +1,3 @@
-
 import cv2
 import numpy as np
 import torch
@@ -7,7 +6,7 @@ from collections import Counter
 import logging
 from model_loader import ModelLoader
 from translation_utils import MarianTranslator
-#from qwen2_VL import Qwen2_VL # تأكد من وجود هذا الاستيراد
+# from qwen2_VL import Qwen2_VL # تأكد من وجود هذا الاستيراد
 from PIL import Image
 from qwen_vl_utils import process_vision_info
 import traceback
@@ -15,7 +14,7 @@ import traceback
 logger = logging.getLogger(__name__)
 
 model_loader = ModelLoader()
-translator = MarianTranslator() # تهيئة المترجم هنا
+translator = MarianTranslator()  # تهيئة المترجم هنا
 
 
 def move_to_device(obj, device):
@@ -32,14 +31,13 @@ def move_to_device(obj, device):
 
 class ActivityRecognizer:
 
-
     def __init__(self):
         try:
             # تحميل نموذج Qwen2-VL فقط
             self.qwen2_vl_proc, self.qwen2_vl_model = model_loader.load_qwen2_vl_model()
             if self.qwen2_vl_model is None:
                 raise Exception("فشل تحميل نموذج Qwen2-VL.")
-            
+
             # device=torch.device("cuda:0")
             self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             # self.device =device  #next(self.qwen2_vl_model.parameters()).device
@@ -52,26 +50,25 @@ class ActivityRecognizer:
             self.device = None
             print(f"❌ فشل تهيئة ActivityRecognizer. فشل تحميل النماذج: {e}   traceback  {traceback.format_exc()}")
 
-
-    def recognize_activity(self,prompt:str, video_path: str, fsp: float, pixels_size: int):
+    def recognize_activity(self, prompt: str, video_path: str, fsp: float, pixels_size: int,max_new_tokens: int=130, temperature: float = 0.7, top_p: float = 0.9, top_k: int = 50, do_sample: bool = True):
         """
         تقوم هذه الدالة بتحليل النشاط والبيئة باستخدام Qwen2-VL.
         يمكنها العمل على إطار واحد أو تجميع الإطارات في دفعات.
         """
         messages = [
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "video",
-                            "video": video_path,  # مسار الفيديو الكامل
-                                "max_pixels": pixels_size * pixels_size,  # تقليل الدقة لتوفير الذاكرة
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "video",
+                        "video": video_path,  # مسار الفيديو الكامل
+                        "max_pixels": pixels_size * pixels_size,  # تقليل الدقة لتوفير الذاكرة
                         "fps": fsp,
-                        },
-                        {"type": "text", "text": prompt},
-                    ],
-                }
-            ]
+                    },
+                    {"type": "text", "text": prompt},
+                ],
+            }
+        ]
 
         # تفريغ ذاكرة قبل الاستدلال
         torch.cuda.empty_cache()
@@ -80,7 +77,7 @@ class ActivityRecognizer:
             messages, tokenize=False, add_generation_prompt=True
         )
         image_inputs, video_inputs = process_vision_info(messages)
-        inputs = self.qwen2_vl_proc( 
+        inputs = self.qwen2_vl_proc(
             text=[text],
             images=image_inputs,
             videos=video_inputs,
@@ -91,20 +88,24 @@ class ActivityRecognizer:
         # device = next(model.parameters()).device
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
 
-        # inputs = inputs.to(self.device) 
+        # inputs = inputs.to(self.device)
         # Inference مع no_grad لتوفير الذاكرة
         with torch.no_grad():
-            generated_ids = self.qwen2_vl_model.generate( 
+            generated_ids = self.qwen2_vl_model.generate(
                 **inputs,
-                max_new_tokens=150
+                max_new_tokens=max_new_tokens,
+                temperature=temperature,
+                top_p=top_p,
+                top_k=top_k,
+                do_sample=do_sample,
             )
         # Trim the generated output to remove the input prompt
         # generated_ids_trimmed = [
         #     out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
         # ]
         generated_ids_trimmed = [
-                 out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs["input_ids"], generated_ids)
-            ]
+            out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs["input_ids"], generated_ids)
+        ]
         # تفريغ فوري بعد الاستدلال
         del inputs  # حذف المدخلات
         torch.cuda.empty_cache()
@@ -113,7 +114,7 @@ class ActivityRecognizer:
             generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
         return output_text[0]
-    
+
     def cleanup(self):
         """تنظيف الموارد"""
         self.qwen2_vl_proc = None
